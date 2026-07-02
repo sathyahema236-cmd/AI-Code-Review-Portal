5453a0cda5e908a59df710edb8d6a440bf5eb2e9
Sathya <sathyhaema236@gmail.com>
2026-07-02 22:28:31 +0530

    Use environment variables for database

--- database/db.py
+++ database/db.py
@@ -1,10 +1,14 @@
+import os
 import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
     connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="welcome",
-        database="ai_code_review_portal"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
     return connection
