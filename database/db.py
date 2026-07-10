98dc7a71b164ce2a320a716b67b7eb22d99843af
Sathya <sathyhaema236@gmail.com>
2026-07-10 21:23:48 +0530

    Connected Aiven MySQL database

--- database/db.py
+++ database/db.py
@@ -1,14 +1,13 @@
 import os
 import mysql.connector
-from dotenv import load_dotenv
-
-load_dotenv()
 
 def get_connection():
     connection = mysql.connector.connect(
         host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        database=os.getenv("DB_NAME"),
+        ssl_disabled=False
     )
     return connection
